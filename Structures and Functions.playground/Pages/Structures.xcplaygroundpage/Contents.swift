14fd7c36a279b40a631db8d9ff7be5ef04fed3a8
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-11-23 13:51:04 -0500

    Improved accuracy of naming related to structures.

--- Structures and Functions.playground/Pages/Structures.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Structures.xcplaygroundpage/Contents.swift
@@ -15,12 +15,11 @@ import Foundation
  
  We could define this using two separate variables:
  
-    var x : Double = 3.0
-    var y : Double = 4.0
+ ![example](example10.png "example")
  
- The downside is that both of these pieces of information define the point, but they are declared separately. Keeping track of information this way can lead to bugs and code that is not very readable.
+ The downside is that *both* of these pieces of information define the point, but they are declared separately. Keeping track of information this way can lead to bugs and code that is not very readable.
  
- We can use a **structure** to connect the *ordinate* (x-value) and the *abscissa* (y-value) to together define a point:
+ We can use a **structure** to connect the *abscissa* (x-value) and the *ordinate* (y-value) to together define a point:
  
  ![example](example2.png "example")
  
@@ -36,7 +35,9 @@ import Foundation
 
  ![example](example3.png "example")
  
- What value is a point initialized with by default?
+ These newly created points are called *instances* of the Point structure.
+ 
+ So, a question: what location on a Cartesian plane is a Point initialized with by default?
  
  */
 
@@ -44,17 +45,19 @@ import Foundation
 
 /*:
  
- You can query a structure to find the value of an individual variable stored within the structure.
+ You can query a structure to find the value of a *property* stored within the structure.
  
  Try typing the following:
  
-    start.x
-    start.y
-    end.x
-    end.y
+ ![example](example11.png "example")
+
+ You should see that the instance of the Point structure named *start* has it's stored property named *x* set to 0.0.
  
- What does the playground tell you?
+ The instance of the Point structure named *end* has it's stored property named *x* set to 3.0.
  
+ What is the value of the stored property named *y* for the *start* instance?
+ 
+ What is the value of the stored property named *y* for the *end* instance?
 */
 
 // Query the points here to find the x and y values separately
@@ -69,7 +72,9 @@ import Foundation
  
  ![example](example5.png "example")
  
- You can copy and paste the function from the earlier page and then make modifications so that the function body uses the points.
+ You can copy and paste the function from the earlier page.
+ 
+ Then, make modifications so that the function body uses *from.x*, *from.y*, et cetera.
  
  The function would now be invoked as follows:
  
@@ -77,7 +82,9 @@ import Foundation
  
  Test your new function to verify that it works correctly and returns accurate values.
  
- Now consider the new code: which version of the *distance* function is cleaner? More readable?
+ Now consider the new code: which version of the *distance* function is cleaner?
+ 
+ When the function is invoked, which version is more readable?
  
  */
 
