65225085bc71cd76b20077fbb5e4bb1f4c5fdf8b
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-11-23 14:05:38 -0500

    Added additional scaffolding for the exercise.

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -10,14 +10,26 @@ import Foundation
 /*:
  ## Exercise
  
- Write a function that determines the shortest distance from a point on a Cartesian plane to a line on a Cartesian plane.
+ Write a function named **shortestDistance** that determines the shortest distance from a point on a Cartesian plane to a line on a Cartesian plane.
+ 
+ The function header should look like this:
+ 
+  ![example](example1.png "example")
  
  You can re-use the **Point** structure and the *distance* function.
  
- You should declare a **Line** structure.
+ You should declare a new **Line** structure.
  
- Recall that the shortest distance from a point to a line is the perpendicular distance.
+ Recall that a line is defined by it's *slope* and it's *vertical intercept*. What stored properties would you create for a **Line** structure?
+ 
+ Recall that slope, in turn, is defined by it's *rise* and *run*. Could you create a **Slope** structure? What stored properties might it define?
+ 
+ Recall that the shortest distance from a point to a line is the *perpendicular* distance from that point to the line.
+ 
+ Here is a [complete example showing how to find the shortest distance from a point to a line](http://russellgordon.ca/lcs/shortest-distance-example.pdf), using symbolic algebra.
+ 
+ Your job is to implement a solution in code that carries out the necessary mathematics. The beautiful part of this effort is that, once you've "taught" the computer how to do this correctly, you will never need to do this type of work manually again. 👍🏽😅🎉
 
 */
 
-// Begin here... once you teach the computer to do this, you will never need to do it by hand again! :)
+// Begin here... 
