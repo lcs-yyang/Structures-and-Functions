10657f7db3d7e23bd78a391eaefb8c5fb992ec84
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2017-11-28 21:55:36 -0500

    done

--- Structures and Functions.playground/Pages/Return Values.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Return Values.xcplaygroundpage/Contents.swift
@@ -21,10 +21,13 @@ import Foundation
  Type the code that you see above. Then run the playground to try it out.
  */
 // Define the function here
-
+func distance(fromX: Double, fromY: Double, toX: Double, toY: Double ) -> Double {
+    return sqrt(pow(fromX - toX, 2) + pow(fromY - toY, 2))
+}
 // Now invoke the function to calculate the distance between (0, 0) and (3, 4)
 // NOTE: Before you run the code, make a prediction about the distance between these two points.
-
+distance(fromX: 0, fromY: 0, toX: 3, toY: 4)
+print(RETURN)
 /*:
  ### Documenting functions
  When we start to add behaviour to our programs using functions, it's important to document how each function works.
